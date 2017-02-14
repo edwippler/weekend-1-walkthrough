48a4514c52ec4268a2b3c902d2a28aef520c5530
Edmund Wippler <edmund@Edmunds-MacBook-Pro.local>
2017-02-14 09:53:36 -0600

    incorporated serializedArray and .notation

--- client.js
+++ client.js
@@ -1,44 +1,56 @@
 $(document).ready(function(){//waits for DOM to completely load
-  $('#submitNewEmployee').on('click', function(){ // event listener on submitNewEmployee button.
-//declaring variables & retrieving values from input boxes.
-   var firstName = $('#firstName').val();
-   var lastName = $('#lastName').val();
-   var idNumber = $('#idNumber').val();
-   var jobTitle = $('#jobTitle').val();
-   var annualSalary = $('#annualSalary').val();
+  $('form').on('submit', function(event){ // event listener on form submission.
+    event.preventDefault(); //stops from changing to a new page.
 
-// add new employee row to the DOM
-   $('#employeeTableBody').append(
-   '<tr>' +
-     '<td>' + firstName + '</td>' +
-     '<td>' + lastName + '</td>' +
-     '<td>' + idNumber + '</td>' +
-     '<td>' + jobTitle + '</td>' +
-     '<td>' + annualSalary + '</td>' +
-     '<td><button class="deleteEmployeeButton" data-salary="' + annualSalary + '">Delete '+ firstName + '</button></td>' +
-   '</tr>'
- );
-// add monthly salary expenses to the DOM
- var newEmployeeMonthlyExpenses = annualSalary / 12;
- var previousMonthlyExpenses = $('#monthlyExpenses').text();
- var totalMonthlyExpenses = parseFloat(previousMonthlyExpenses) + newEmployeeMonthlyExpenses;
- $('#monthlyExpenses').text(totalMonthlyExpenses);
+    // create an array of the inputs - the inputs are converted to objects.
+    console.log('form values: ', $(this).serializeArray());
 
- // clear our input boxes
- $('.employeeFormInput').val('');
+    var submissionArray = $(this).serializeArray();
+    var newEmployeeObjet = {}; // [firstName: 'Luke', lastName: 'Schlangen']
 
- });
+    submissionArray.forEach(function(inputField){
+      // newEmployeeObjet is {}
+      newEmployeeObjet[inputField.name] = inputField.value;
+      // newEmployeeObjet.firstName = luke;
+      // newEmployeeObjet is {firstName: 'Luke'}
+      // 2nd time through newEmployeeObjet is {firstName: 'Luke', lastName: 'Schlangen'}
+    });
+    console.log(newEmployeeObjet);
 
-// adding event listener for clicking delete button
- $('#employeeTableBody').on('click', '.deleteEmployeeButton', function(){
-   //removing employee salary from total.
-   var deletedEmployeeSalary = $(this).data('salary');
-   var deletedEmployeeMonthlySalary = deletedEmployeeSalary / 12
-   var previousMonthlyExpenses = $('#monthlyExpenses').text();
-   var updatedMonthlyExpenses = previousMonthlyExpenses - deletedEmployeeMonthlySalary; //does not need parseFloat because of type coercion.
-   $('#monthlyExpenses').text(updatedMonthlyExpenses);
-  //  seleting and deleting employee row from table.
-   $(this).closest('tr').remove();
 
- })
+
+    // add new employee row to the DOM
+    $('#employeeTableBody').append(
+      '<tr>' +
+      '<td>' + newEmployeeObjet.firstName + '</td>' +
+      '<td>' + newEmployeeObjet.lastName + '</td>' +
+      '<td>' + newEmployeeObjet.idNumber + '</td>' +
+      '<td>' + newEmployeeObjet.jobTitle + '</td>' +
+      '<td>' + newEmployeeObjet.annualSalary + '</td>' +
+      '<td><button class="deleteEmployeeButton" data-salary="' + newEmployeeObjet.annualSalary + '">Delete '+ newEmployeeObjet.firstName + '</button></td>' +
+      '</tr>'
+    );
+    // add monthly salary expenses to the DOM
+    var newEmployeeMonthlyExpenses = newEmployeeObjet.annualSalary / 12;
+    var previousMonthlyExpenses = $('#monthlyExpenses').text();
+    var totalMonthlyExpenses = parseFloat(previousMonthlyExpenses) + newEmployeeMonthlyExpenses;
+    $('#monthlyExpenses').text(totalMonthlyExpenses);
+
+    // clear our input boxes
+    $('.employeeFormInput').val('');
+
+  });
+
+  // adding event listener for clicking delete button
+  $('#employeeTableBody').on('click', '.deleteEmployeeButton', function(){
+    //removing employee salary from total.
+    var deletedEmployeeSalary = $(this).data('salary');
+    var deletedEmployeeMonthlySalary = deletedEmployeeSalary / 12
+    var previousMonthlyExpenses = $('#monthlyExpenses').text();
+    var updatedMonthlyExpenses = previousMonthlyExpenses - deletedEmployeeMonthlySalary; //does not need parseFloat because of type coercion.
+    $('#monthlyExpenses').text(updatedMonthlyExpenses);
+    //  seleting and deleting employee row from table.
+    $(this).closest('tr').remove();
+
+  })
 });
